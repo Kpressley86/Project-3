f65d05a33896830accad21bb1ec31b6ee2f090c3
Kelly Pressley <kellypressley86@gmail.com>
2020-01-18 12:21:35 -0700

    added auth0 to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,50 @@
-import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useAuth0 } from './contexts/auth0-context';
+import Header from './components/header/Header';
+// import logo from "./logo.svg";
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+function App() {
+  const { isLoading, user, loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <>
+      <Header />
+
+      <div className="hero is-info is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            {!isLoading && !user && (
+              <>
+                <h1>Click Below!</h1>
+                <button onClick={loginWithRedirect} className="button is-danger">
+                  Login
+              </button>
+              </>
+            )}
+            
+            {!isLoading && user && (
+              <>
+                <h1>You are logged in!</h1>
+                <p>Hello {user.name}</p>
+
+                {user.picture && <img src={user.picture} alt="My Avatar" />}
+                <hr />
+
+                <button
+                  onClick={() => logout({ returnTo: window.location.origin })}
+                  className="button is-small is-dark"
+                >
+                  Logout
+          </button>
+              </>
+            )}
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
-  }
+    </>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from './contexts/auth0-context';
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Auth0Provider>
+        <App />
+    </Auth0Provider>,
+    document.getElementById("root"));
 registerServiceWorker();
