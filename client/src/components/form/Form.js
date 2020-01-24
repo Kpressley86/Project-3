78be7bf73f0ea48a345da43dce19b40864b0d8a7
Basem Jaber <basemjaber32@gmail.com>
2020-01-23 21:15:24 -0700

    updated css on form,

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -33,28 +33,35 @@ class Form extends Component {
     render() {
         return(
             <div>
-                <form>
+                <form className="form">
                     <input
+                        class ="form_label"
                         type="text"
                         name="budget"
                         placeholder="Type your budget"
                         value={this.state.addBudget}
                         onChange={(e) => this.handleInputChange(e)}
                     /><br /> 
+                    <br></br>
                      <input
+                        class ="form_field"
                         type="text"
                         name="expense"
                         placeholder="Type your expense"
                         value={this.state.addExpense}
                         onChange={(e) => this.handleInputChange(e)}
                     /><br />
+                    <br></br>
                     <input
+                        class="form_field"
                         type="text"
                         name="leftoverfund"
                         placeholder="Type your leftoverfund"
                         value={this.state.letfoverfund}
                         onChange={(e) => this.handleInputChange(e)}
                     />
+                    <br></br>
+                    <br></br>
                     <input type="submit" value="Submit" />
                 </form>
             </div>
